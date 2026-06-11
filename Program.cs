a86be7b993e1cf3190ae3803d4781326b152e955
julia <julia_oliveira160@estudante.sesisenai.org.br>
2026-06-11 20:45:25 -0300

    q4

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
-﻿Console.WriteLine("Informe a temperatura atual:");
-double temperatura = double.Parse(Console.ReadLine());
+﻿Console.WriteLine("Informe o valor total da compra:");
+double valorOriginal = double.Parse(Console.ReadLine());
 
-if (temperatura < 18)
+double desconto = 0;
+
+if (valorOriginal >= 200)
 {
-    Console.WriteLine("Ambiente frio.");
+    desconto = valorOriginal * 0.10;
 }
-else if (temperatura <= 26)
-{
-    Console.WriteLine("Ambiente confortável.");
-}
-else
-{
-    Console.WriteLine("Ambiente quente.");
-}
+
+double valorFinal = valorOriginal - desconto;
+
+Console.WriteLine($"Valor original: R$ {valorOriginal:F2}");
+Console.WriteLine($"Desconto aplicado: R$ {desconto:F2}");
+Console.WriteLine($"Valor final: R$ {valorFinal:F2}");
