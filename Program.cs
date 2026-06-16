f9816dcfba0b465c601c2e7351d627ad1a0a903e
julia <julia_oliveira160@estudante.sesisenai.org.br>
2026-06-15 21:10:11 -0300

    q1

--- Program.cs
+++ Program.cs
@@ -1,42 +1,11 @@
-﻿Console.Write("Informe a quantidade inicial em estoque: ");
-int estoque = int.Parse(Console.ReadLine());
+﻿Console.Write("Informe a idade do visitante: ");
+int idade = int.Parse(Console.ReadLine());
 
-int operacao;
-
-do
+if (idade >= 16)
 {
-    Console.Write("Escolha a operação: ");
-    operacao = int.Parse(Console.ReadLine());
-
-    if (operacao == 1)
-    {
-        Console.Write("Informe a quantidade movimentada: ");
-        int quantidade = int.Parse(Console.ReadLine());
-
-        estoque += quantidade;
-
-        Console.WriteLine($"Entrada registrada. Estoque atual: {estoque}");
-    }
-    else if (operacao == 2)
-    {
-        Console.Write("Informe a quantidade movimentada: ");
-        int quantidade = int.Parse(Console.ReadLine());
-
-        if (quantidade <= estoque)
-        {
-            estoque -= quantidade;
-            Console.WriteLine($"Saída registrada. Estoque atual: {estoque}");
-        }
-        else
-        {
-            Console.WriteLine("Quantidade insuficiente em estoque.");
-        }
-    }
-    else if (operacao == 3)
-    {
-        Console.WriteLine($"Estoque atual: {estoque}");
-    }
-
-} while (operacao != 4);
-
-Console.WriteLine($"Estoque final: {estoque}");
+    Console.WriteLine("Entrada liberada.");
+}
+else
+{
+    Console.WriteLine("Entrada permitida somente com responsável.");
+}
